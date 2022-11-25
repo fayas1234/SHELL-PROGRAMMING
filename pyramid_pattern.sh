27949c8980cf1dcde3414a4d2860c736d9d749fe
fayas1234 <94542226+fayas1234@users.noreply.github.com>
2022-11-25 20:42:02 +0530

    Create pyramid_pattern.sh

---
+++ pyramid_pattern.sh
@@ -0,0 +1,34 @@
+#Program to print a pyramid using *
+
+#!/usr/bin/bash
+
+row=0
+
+for((i=4;i>=1;i--))
+do
+	for((j=0;j<i-1;j++))
+	do
+		echo -n " "
+	done
+	
+	for((j=0;j<(2*$row+1);j++)) #can use just row without $
+	do
+		echo -n "*"
+	done
+	echo
+	
+	let row=$row+1 #let row++
+done
+
+
+<<comment
+
+This script will print the pattern:
+
+   *
+  ***
+ *****
+*******
+
+
+comment
